8e72259008da7d8603becaeb247067162d6d5c4d
thomas-james-85 <tom@unifabs.eu>
2025-03-10 12:56:46 +0000

    fixed hydration isseus

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Don't attempt to import server-only modules on the client side
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+        net: false,
+        tls: false,
+        dns: false,
+        child_process: false
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
